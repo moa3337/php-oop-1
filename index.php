59e7bb7e773ea410a3c3d360ce0dfdcf4a4d0bb3
moa3337 <moa-x@hotmail.com>
2023-03-22 15:57:26 +0100

    mtodo & costruttore

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+
+class Movie {
+    public $title;
+    public $director;
+    public $year;
+    public $rating; 
+    public $vote;
+
+    /*public function setVote($rating) {
+        if($rating > 7.5) {
+            $this->vote = "good movie";
+        }
+    }
+
+    public function getVote() {
+        return $this->vote;
+    }*/
+
+    function __construct($_vote) {
+        $this->vote = $_vote;
+    }
+}
+
+$movie1 = new Movie(5);
+$movie1->title = "Nomadlend";
+$movie1->director = "Chloè Zhao";
+$movie1->year = "2019";
+$movie1->rating = 9.5;
+echo $movie1->vote;
+//$vote_movie1 = $movie1->getVote();
+
+$movie2 = new Movie();
+$movie2->title = "Creed III";
+$movie2->director = "Mchael B. Jordan";
+$movie2->year = "2023";
+$movie2->rating = 7.1;
+//$vote_movie2 = $movie2->getVote();
+
+var_dump($movie1);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
